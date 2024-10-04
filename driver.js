cb0068b505fc9284af1f83a3fcab05cdf81934b6
name <rk6318633@gmail.com>
2024-10-04 18:29:25 +0530

    fs demo

--- driver.js
+++ driver.js
@@ -1,3 +1,26 @@
- const {linearsearch,binarysearch} = require('./searching.js'); 
- console.log(linearsearch)
- console.log(binarysearch)
+//  const {linearsearch,binarysearch} = require('./searching.js'); 
+//  console.log(linearsearch)
+//  console.log(binarysearch)
+
+const fs = require('fs/promises');
+
+async function readSampleFile() {
+    const data = await fs.readFile('./sample.txt','utf-8');
+    console.log(data);
+    
+}
+
+async function writesampleFile() {
+    
+    await fs.writeFile('./sample.txt','Hello world1')
+}
+
+async function appendSampleFile() {
+    await fs.appendFile('./sample.txt','Hello world')
+}
+
+appendSampleFile()
+
+// writesampleFile()
+
+// readSampleFile()
